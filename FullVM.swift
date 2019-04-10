cb3d5a5c5662addd5b3414a38e38ccdef415ef74
jShen118 <47192295+jShen118@users.noreply.github.com>
2019-04-10 18:42:31 -0400

    works now
    for some of the out and print instructions it no longer adds to the console

--- FullVM.swift
+++ FullVM.swift
@@ -1,43 +1,39 @@
-//
-//  FullVM.swift
-//  PartialVM
-//
-//  Created by Nicholas Hatzis-Schoch on 4/6/19.
-//  Copyright © 2019 Slick Games. All rights reserved.
-//
-
 import Foundation
 
 struct FullVM {
-    var instructionPointer: Int = 0
-    var memory = [Int](repeating: 0, count: 1000)
-    var size: Int = 0
-    var startAddress : Int = 1
+    //runs the assembly binary code for the Doubles Program
+    var instructionPointer = 0
+    var memory = Array(repeating: 0, count: 10000)
     var binary = [0]
+    var size = 0
+    var startAddress = 0
     var registers = Array(repeating: 0, count: 10)
-    let support = Support()
     var statusFlag = StatusFlag()
+    
+    let support = Support()
     var userInput = ""
     var wasCrashed = false
     
-    //unsure about the mechanics of this one:
     var stackRegister = 0
     var console: String = ""
     
-    init(){}/*mem: [Int]) {
-        memory = mem
-        size = mem[0]
-        startAddress = mem[1]
-        instructionPointer = startAddress + 2 //the plus 2 is because memory actually starts
-        //from memory[2] as opposed to memory[0] due to length and startAddress taking 2 spaces
+    /*
+    init(binary: [Int]) {
+        size = binary[0]
+        startAddress = binary[1]
+        instructionPointer = startAddress
+        for n in 2..<binary.count { //binary[0] and binary[1] not part of memory
+            memory[n - 2] = binary[n]
+        }
     }*/
+    init() {}
     
     mutating func run() {
-        print("Welcome to SAP!")
+        print("Welcome to the binary to ouput virtual machine!")
         help()
         print(">", terminator: "")
         userInput = readLine()!
-        while userInput != "quit"{
+        while userInput != "quit" {
             var splitInput = support.splitStringIntoParts(userInput)
             switch splitInput[0] {
             //for each case first the number of arguments is checked and then whether the type of the argument is as expected
@@ -61,67 +57,20 @@ struct FullVM {
             userInput = readLine()!
         }
     }
+    
     mutating func executeBinary() {
-        instructionPointer = startAddress + 1
+        instructionPointer = startAddress
         while(pointerIsInMemoryBounds()) {
             switch memory[instructionPointer] {
-            case 0: wasCrashed = false; return //halt
-            case 1: clrr(memory[instructionPointer + 1])
-            case 2: clrx(memory[instructionPointer + 1])
-            case 3: clrm(memory[instructionPointer + 1])
-            case 4: clrb(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 5: movir(memory[instructionPointer + 1], memory[instructionPointer + 2])
+            case 0: wasCrashed = false; return
             case 6: movrr(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 7: movrm(memory[instructionPointer + 1], memory[instructionPointer + 2])
             case 8: movmr(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 9: movxr(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 10: movar(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 11: movb(memory[instructionPointer + 1], memory[instructionPointer + 2], memory[instructionPointer + 3])
             case 12: addir(memory[instructionPointer + 1], memory[instructionPointer + 2])
             case 13: addrr(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 14: addmr(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 15: addxr(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 16: subir(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 17: subrr(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 18: submr(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 19: subxr(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 20: mulir(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 21: mulrr(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 22: mulmr(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 23: mulxr(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 24: divir(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 25: divrr(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 26: divmr(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 27: divxr(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 28: jmp(memory[instructionPointer + 1])
-            case 29: sojz(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 30: sojnz(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 31: aojz(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 32: aojnz(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 33: cmpir(memory[instructionPointer + 1], memory[instructionPointer + 2])
             case 34: cmprr(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 35: cmpmr(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 36: jmpn(memory[instructionPointer + 1])
-            case 37: jmpz(memory[instructionPointer + 1])
-            case 38: jmpp(memory[instructionPointer + 1])
-            case 39: jsr(memory[instructionPointer + 1])
-            case 40: ret()
-            case 41: push(memory[instructionPointer + 1])
-            case 42: pop(memory[instructionPointer + 1])
-            case 43: stackc(memory[instructionPointer + 1])
-            case 44: outci(memory[instructionPointer + 1])
             case 45: outcr(memory[instructionPointer + 1])
-            case 46: outcx(memory[instructionPointer + 1])
-            case 47: outcb(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 48: readi(memory[instructionPointer + 1], memory[instructionPointer + 2])
             case 49: printi(memory[instructionPointer + 1])
-            case 50: readc(memory[instructionPointer + 1])
-            case 51: readln(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 52: brk()
-            case 53: movrx(memory[instructionPointer + 1], memory[instructionPointer + 2])
-            case 54: movxx(memory[instructionPointer + 1], memory[instructionPointer + 2])
             case 55: outs(memory[instructionPointer + 1])
-            case 56: nop()
             case 57: jmpne(memory[instructionPointer + 1])
             default:
                 print("Error: Nonexistent instruction called, instruction # \(memory[instructionPointer]) does not exist")
@@ -134,7 +83,9 @@ struct FullVM {
     }
 }
 
-extension FullVM { //extension for helper functions
+
+//extension for helpers
+extension FullVM {
     func getString(_ labelAddress: Int)->String {
         var toReturn = ""
         let stringLength = memory[labelAddress]
@@ -188,14 +139,12 @@ extension FullVM { //extension for helper functions
         return args.count - 1
     }
 }
+
+
 //extension for executing instructions
 extension FullVM {
     /*
      IMPORTANT NOTES:
-     any time that a label address is taken in as a parameter it needs to
-     have 2 added it to access the desired memory location since length and
-     start address hold the 2 first positions in the memory array
-     
      print statements have the "terminator: String" parameter since swift
      automatically makes the terminator "\n" if not called and we do not want
      to skip any lines when printing except for when outcr-ing #10
@@ -258,6 +207,7 @@ extension FullVM {
     
     mutating func movar(_ labelAddress: Int, _ rIndex: Int) { //10
         registers[rIndex] = labelAddress
+        instructionPointer += 3
     }
     
     mutating func movb(_ r1Index: Int, _ r2Index: Int, _ r3Index: Int) { //11 INCOMPLETE
@@ -376,25 +326,25 @@ extension FullVM {
     }
     
     mutating func cmpir(_ int: Int, _ rIndex: Int) { //33
-        
         instructionPointer += 3
+        
     }
     
     mutating func cmprr(_ r1Index: Int, _ r2Index: Int) { //34
+        instructionPointer += 3
         let r1Int = registers[r1Index]
         let r2Int = registers[r2Index]
         if r1Int == r2Int {statusFlag.makeEqual(); return}
         if r1Int > r2Int {statusFlag.makeMoreThan(); return}
         statusFlag.makeLessThan()
-        instructionPointer += 3
     }
     
     mutating func cmpmr(_ labelAddress: Int, _ rIndex: Int) { //35
+        instructionPointer += 3
         let rIndex = registers[rIndex]
         if rIndex == memory[labelAddress] {statusFlag.makeEqual(); return}
         if rIndex > memory[labelAddress] {statusFlag.makeMoreThan(); return}
         statusFlag.makeLessThan()
-        instructionPointer += 3
     }
     
     mutating func jmpn(_ labelAddress: Int) { //36
@@ -451,7 +401,6 @@ extension FullVM {
     
     mutating func outcr(_ rIndex: Int) { //45
         print(support.unicodeValueToCharacter(registers[rIndex]), terminator: "")
-        console += String(support.unicodeValueToCharacter(registers[rIndex]))
         instructionPointer += 2
     }
     
@@ -482,7 +431,6 @@ extension FullVM {
     
     mutating func printi(_ rIndex: Int) { //49
         print(registers[rIndex], terminator: "")
-        console += String(registers[rIndex])
         instructionPointer += 2
     }
     
@@ -522,9 +470,8 @@ extension FullVM {
     }
     
     mutating func outs(_ labelAddress: Int) { //55
-        let toPrint = getString(labelAddress + 2)
+        let toPrint = getString(labelAddress)
         print(toPrint, terminator: "")
-        console += toPrint
         instructionPointer += 2
     }
     
@@ -534,7 +481,7 @@ extension FullVM {
     
     mutating func jmpne(_ labelAddress: Int) { //57
         if statusFlag.status != 0 {
-            instructionPointer = labelAddress + 2
+            instructionPointer = labelAddress
         } else {
             instructionPointer += 2
         }
