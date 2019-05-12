58b7267b9382fee408452c5dbb56cdc28e3c8574
jShen118 <47192295+jShen118@users.noreply.github.com>
2019-05-12 16:28:24 -0400

    Create TokenType.swift

---
+++ TokenType.swift
@@ -0,0 +1,11 @@
+enum TokenType: Int {
+    case Register 
+    case LabelDefinition
+    case Label
+    case ImmediateString
+    case ImmediateInteger
+    case ImmediateTuple
+    case Instruction
+    case Directive
+    case BadToken
+}
