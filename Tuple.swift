90f953bc0796a93259d53e397b2380c4dc6eb57c
slickboi <38701213+SlickNick17@users.noreply.github.com>
2019-05-12 21:32:58 -0400

    Update Tuple.swift

--- Tuple.swift
+++ Tuple.swift
@@ -1,10 +1,19 @@
+import Foundation
+
 struct Tuple: CustomStringConvertible {
     let currentState: Int
-    let inputCharacter: Int
+    let inputCharacter: Character
     let newState: Int
-    let outputCharacter: Int
-    let direction: Int
+    let outputCharacter: Character
+    let direction: Character
     
+    init(cs: Int, ic: Character, ns: Int, oc: Character, di: Character){
+        self.currentState = cs
+        self.inputCharacter = ic
+        self.newState = ns
+        self.outputCharacter = oc
+        self.direction = di
+    }
     var description: String {
         return "{cs: \(currentState), ic: \(inputCharacter), ns: \(newState), oc: \(outputCharacter), di: \(direction)}"
     }
