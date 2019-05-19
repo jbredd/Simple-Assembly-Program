3b687da5b537d5565d9807c354f5fc0fedf907da
jShen118 <47192295+jShen118@users.noreply.github.com>
2019-05-19 17:13:35 -0400

    now has own chunkinizer function, it works

--- Line.swift
+++ Line.swift
@@ -1,11 +1,50 @@
-struct Line {
+import Foundation
+
+struct Line: CustomStringConvertible {
     var characters: [Character]
     var chunks = [String]()
     var tokens = [Token]()
     var number: Int
+    var tokenizer = Tokenizer()
     
     init(_ number: Int, _ line: String) {
         characters = Array(line)
         self.number = number
+        chunkinize(characters)
+        tokens = tokenizer.tokenizeChunks(chunks)
+    }
+    
+    mutating func chunkinize(_ characters: [Character]) {
+        var ignoreSpaces = false
+        var chunk = ""
+        for i in 0..<characters.count {
+            if !ignoreSpaces {
+                if characters[i] == " " {
+                    chunks.append(chunk)
+                    chunk = ""
+                } else {
+                    chunk += String(characters[i])
+                    if characters[i] == "\"" || characters[i] == "\\" {ignoreSpaces = true}
+                    if i == characters.count - 1 {chunks.append(chunk)}
+                }
+            } else {
+                chunk += String(characters[i])
+                if characters[i] == "\"" || characters[i] == "\\" {
+                    ignoreSpaces = false
+                }
+            }
+        }
+    }
+    
+    func printLine() {
+        for c in chunks {print("\(c) ")}
+    }
+    
+    var description: String {
+        var description = "Line #\(number):\n"
+        description += "  characters: \(characters)\n"
+        description += "  chunks: \(chunks)\n"
+        description += "  tokens: \(tokens)\n"
+        return description
     }
 }
