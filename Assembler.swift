2f2602a573da46581797c97201728d6b19138996
jShen118 <47192295+jShen118@users.noreply.github.com>
2019-05-26 17:15:06 -0400

    can now store multiple programs

--- Assembler.swift
+++ Assembler.swift
@@ -8,8 +8,9 @@
 
 import Foundation
 
+
 struct Assembler{
-    var program: Program? = nil
+    var programs = [Program]()
     
     var instructionArgs: [String: [TokenType]] = [:]
     var directiveArgs: [String : [TokenType]] = [:]
@@ -17,29 +18,39 @@ struct Assembler{
     init() {fillDictionary()}
     
     
-    mutating func read()-> Bool {
-        if Support.readTextFile(program!.path).fileText == nil {
-            print(Support.readTextFile(program!.path).message!)
+    mutating func read(_ path: String)-> Bool {
+        if Support.readTextFile(path).fileText == nil {
+            print(Support.readTextFile(path).message!)
             return false
         }
-        let fileContent = Support.readTextFile(program!.path).fileText!
+        programs.append(Program(path))
+        let fileContent = Support.readTextFile(path).fileText!
         let inputCode = Support.splitStringIntoLines(fileContent)
         for i in 0..<inputCode.count {
             //print("\(i + 1)     \(inputCode[i])")
             if inputCode[i].count > 0 {
-                program!.lines.append(Line(i + 1, inputCode[i]))
+                programs[getProgramIndex(path)!].lines.append(Line(i + 1, inputCode[i]))
             }
         }
         return true
     }
     
-    
-    mutating func assemble(_ path: String) {
-        program = Program(path)
-        passOne()
-        if !program!.legal {print("...Assembly was unsuccessful"); return}
-        passTwo()
+    mutating func assemble(_ pathSpecs: String, _ name: String) {
+        passOne(pathSpecs + name + ".txt")
+        if !programs[getProgramIndex(pathSpecs + name + ".txt")!].legal {
+            print("...Assembly was unsuccessful"); return
+        }
+        passTwo(pathSpecs + name + ".txt")
         print("...Assembly was successful")
+    }
+    
+    func getProgramIndex(_ path: String)-> Int? {
+        for i in 0..<programs.count {
+            if programs[i].path == path {
+                return i
+            }
+        }
+        return nil
     }
     
     mutating func fillDictionary() {
@@ -47,6 +58,10 @@ struct Assembler{
         directiveArgs[".string"] = [.ImmediateString]
         directiveArgs[".integer"] = [.ImmediateInteger]
         directiveArgs[".tuple"] = [.ImmediateTuple]
+        directiveArgs[".Start"] = [.Label]
+        directiveArgs[".String"] = [.ImmediateString]
+        directiveArgs[".Integer"] = [.ImmediateInteger]
+        directiveArgs[".Tuple"] = [.ImmediateTuple]
         instructionArgs["halt"] = []
         instructionArgs["clrr"] = [.Register]
         instructionArgs["clrx"] = [.Register]
@@ -107,11 +122,12 @@ struct Assembler{
         instructionArgs["jmpne"] = [.Label]
     }
     
-    func printSymVal() {
-        if program == nil {print("Please assemble a program first"); return}
+    func printSymVal(_ path: String) {
+        let pIndex = getProgramIndex(path)
+        if pIndex == nil {print("Please assemble the program first"); return}
         print("Symbol table: \n")
-        for (k, _) in program!.symVal {
-            print("\(Support.buffer(Support.removeColon(k), 22)) \(program!.symVal[k]!)")
+        for (k, _) in programs[pIndex!].symVal {
+            print("\(Support.buffer(Support.removeColon(k), 22)) \(programs[pIndex!].symVal[k]!)")
         }
     }
 }
@@ -119,23 +135,25 @@ struct Assembler{
 // PASS ONE
 extension Assembler{
     //passOne makes symVal and checks for legality
-    mutating func passOne() {
-        if !read() {return}
-        program!.legal = true
-        makeSymVal(program!.lines)
-        for l in program!.lines {
+    mutating func passOne(_ path: String) {
+        if !read(path) {return}
+        let pIndex = getProgramIndex(path)
+        programs[pIndex!].legal = true
+        makeSymVal(path, programs[pIndex!].lines)
+        for l in programs[pIndex!].lines {
             print(l, terminator: "")
-            if !isLegal(l).0 {program!.legal = false}
-            print(isLegal(l).1)
+            if !isLegal(path, l).0 {programs[pIndex!].legal = false}
+            print(isLegal(path, l).1)
         }
     }
     
-    mutating func makeSymVal(_ lines: [Line]) { //WRONG
-        var memLocation = -1 //to account for .start label not taking up a space
+    mutating func makeSymVal(_ path: String, _ lines: [Line]) { //WRONG
+        let pIndex = getProgramIndex(path)
+        var memLocation = -1 //to account for .start label address not taking up a space
         for l in lines {
             for i in 0..<l.tokens.count {
                 switch l.tokens[i].type {
-                case .LabelDefinition: program!.symVal[l.chunks[i]] = memLocation
+                case .LabelDefinition: programs[pIndex!].symVal[l.chunks[i]] = memLocation
                 case .ImmediateInteger: memLocation += 1
                 case .ImmediateString: memLocation += l.chunks[i].count - 1
                 case .ImmediateTuple: memLocation += 5
@@ -149,15 +167,15 @@ extension Assembler{
     }
     
     // returns (args are legal, error message)
-    func isLegal(_ line: Line)-> (Bool, String) {
+    func isLegal(_ path: String, _ line: Line)-> (Bool, String) {
         let chunks = line.chunks
         let tokens = line.tokens
         if tokens.count >= 1 {
             switch tokens[0].type {
-            case .Instruction: return validInstructionArgs(line, 0)
+            case .Instruction: return validInstructionArgs(path, line, 0)
             case .Directive: return validDirectiveArg(line, 0)
             case .LabelDefinition:
-                if tokens[1].type == .Instruction {return validInstructionArgs(line, 1)}
+                if tokens[1].type == .Instruction {return validInstructionArgs(path, line, 1)}
                 if tokens[1].type == .Directive {return validDirectiveArg(line, 1)}
                 return (false, "\n..........A Label Definition must be followed by an Instruction or a Directive")
             case .BadToken: return (false, "\n..........\(chunks[0]) is a Bad Token")
@@ -167,12 +185,12 @@ extension Assembler{
     }
     
     //(args are legal, error message), the start arg is the index of the instruction token
-    func validInstructionArgs(_ line: Line, _ start: Int)-> (Bool, String) {
+    func validInstructionArgs(_ path: String, _ line: Line, _ start: Int)-> (Bool, String) {
         let expected = instructionArgs[line.chunks[start]]
         if expected != nil{
             if line.tokens.count - start > expected!.count + 1 {return (false, "\n..........\(line.chunks[start]) has too many arguments")}
             for i in 0..<expected!.count {
-                if expected![i] == .Label && !labelExists(line.chunks[i + start + 1]) {return (false, "\n..........Label \"\(line.chunks[start + i + 1])\" has not been defined")}
+                if expected![i] == .Label && !labelExists(path, line.chunks[i + start + 1]) {return (false, "\n..........Label \"\(line.chunks[start + i + 1])\" has not been defined")}
                 if line.tokens[start + i + 1].type != expected![i] {
                     return (false, "\n..........Instruction \(line.chunks[start]) arguments are not as expected")
                 }
@@ -190,38 +208,40 @@ extension Assembler{
         return (false, "\n..........\(line.chunks[start]) should take in an \(expected![0])")
     }
     
-    func labelExists(_ label: String)-> Bool {
-        return program!.symVal[label + ":"] != nil
+    func labelExists(_ path: String, _ label: String)-> Bool {
+        return programs[getProgramIndex(path)!].symVal[label + ":"] != nil
     }
 }
 
 // PASS TWO
 extension Assembler {
     // pass two makes translates assembly to binary
-    mutating func passTwo() {
-        if !program!.legal {return}
-        translate()
+    mutating func passTwo(_ path: String) {
+        if !programs[getProgramIndex(path)!].legal {return}
+        translate(path)
     }
     
-    func printBin() {
-        if program == nil {print("Please assemble a program first"); return}
-        print(program!.length)
-        print(program!.start)
-        for i in 0..<program!.mem.count {print("\(program!.mem[i])")}
+    func printBin(_ path: String) {
+        let pIndex = getProgramIndex(path)
+        if pIndex == nil {print("Please assemble the program first"); return}
+        print(programs[pIndex!].length)
+        print(programs[pIndex!].start)
+        for i in 0..<programs[pIndex!].mem.count {print("\(programs[pIndex!].mem[i])")}
         print("\n\n\n")
     }
     
-    mutating func translate() {
-        program!.mem = [Int]()
-        for l in program!.lines {
+    mutating func translate(_ path: String) {
+        let pIndex = getProgramIndex(path)
+        programs[pIndex!].mem = [Int]()
+        for l in programs[pIndex!].lines {
             for i in 0..<l.tokens.count {
                 switch l.tokens[i].type {
-                case .Register: program!.mem.append(Translator.registers[l.chunks[i]]!)
-                case .ImmediateString: for b in translateString(l.tokens[i].stringValue!) {program!.mem.append(b)}
-                case .ImmediateInteger: program!.mem.append(l.tokens[i].intValue!)
-                case .ImmediateTuple: for b in translateTuple(l.tokens[i]) {program!.mem.append(b)}
-                case .Instruction: program!.mem.append(Translator.instructions[l.chunks[i]]!)
-                case .Label: program!.mem.append(program!.symVal[l.chunks[i] + ":"]!)
+                case .Register: programs[pIndex!].mem.append(Translator.registers[l.chunks[i]]!)
+                case .ImmediateString: for b in translateString(l.tokens[i].stringValue!) {programs[pIndex!].mem.append(b)}
+                case .ImmediateInteger: programs[pIndex!].mem.append(l.tokens[i].intValue!)
+                case .ImmediateTuple: for b in translateTuple(l.tokens[i]) {programs[pIndex!].mem.append(b)}
+                case .Instruction: programs[pIndex!].mem.append(Translator.instructions[l.chunks[i]]!)
+                case .Label: programs[pIndex!].mem.append(programs[pIndex!].symVal[l.chunks[i] + ":"]!)
                 case .Directive: print("", terminator: "") //does nothing
                 case .LabelDefinition: print("", terminator: "") //does nothing
                 default: print("this should never be printed as pass one should vet for legality")
@@ -230,9 +250,9 @@ extension Assembler {
         }
         //.start location already at beginning due to label locations
         //being put into memory whenever they are encountered
-        program!.start = program!.mem[0]
-        program!.length = program!.mem.count - 1
-        program!.mem.remove(at: 0)
+        programs[pIndex!].start = programs[pIndex!].mem[0]
+        programs[pIndex!].length = programs[pIndex!].mem.count - 1
+        programs[pIndex!].mem.remove(at: 0)
     }
     // \0 _ 0 _ r\
     mutating func translateTuple(_ token: Token)->[Int] {
@@ -255,6 +275,7 @@ extension Assembler {
         return binary
     }
     
+    
     func getIgnore(_ tokens: [Token])-> Int {
         var toIgnore = 0
         for t in tokens {
@@ -265,45 +286,47 @@ extension Assembler {
         return toIgnore
     }
     
-    func getMemContents(_ address: Int, _ l: Line)-> String {
+    func getMemContents(_ path: String, _ address: Int, _ l: Line)-> String {
+        let pIndex = getProgramIndex(path)
         var memContents = ""
-        if address >= program!.length {return "\n"}
+        if address >= programs[pIndex!].length {return "\n"}
         for n in 0..<l.tokens.count {
             switch l.tokens[n].type {
             case .ImmediateString:
-                for i in 0..<program!.mem[address] {
+                for i in 0..<programs[pIndex!].mem[address] {
                     if i <= 3 {
-                        memContents += " \(program!.mem[address + i])"
+                        memContents += " \(programs[pIndex!].mem[address + i])"
                     }
                 }
             case .ImmediateTuple:
                 for i in 0..<4 {
-                    memContents += " \(program!.mem[address + i])"
+                    memContents += " \(programs[pIndex!].mem[address + i])"
                 }
             case .ImmediateInteger:
-                memContents += " \(program!.mem[address + n - getIgnore(l.tokens)])"
+                memContents += " \(programs[pIndex!].mem[address + n - getIgnore(l.tokens)])"
             case .Label:
-                if l.chunks[0] != ".start" {
-                    memContents += " \(program!.mem[address + n - getIgnore(l.tokens)])"
+                if l.chunks[0] != ".start" && l.chunks[0] != ".Start" {
+                    memContents += " \(programs[pIndex!].mem[address + n - getIgnore(l.tokens)])"
                 }
             case .Instruction:
-                memContents += " \(program!.mem[address + n - getIgnore(l.tokens)])"
+                memContents += " \(programs[pIndex!].mem[address + n - getIgnore(l.tokens)])"
             case .Register:
-                memContents += " \(program!.mem[address + n - getIgnore(l.tokens)])"
+                memContents += " \(programs[pIndex!].mem[address + n - getIgnore(l.tokens)])"
             default: print("", terminator: "")
             }
         }
         return "\(memContents)"
     }
     
-    func printLst() {
-        if program == nil {print("Please assemble a program first"); return}
+    func printLst(_ path: String) {
+        let pIndex = getProgramIndex(path)
+        if pIndex == nil {print("Please assemble the program first"); return}
         var toPrint = ""
         var memLocation = 0
         var memContents = " "
-        for l in program!.lines {
+        for l in programs[pIndex!].lines {
             //must getMemContents before changing memLocation since memLocation when printed
-            memContents = getMemContents(memLocation, l)
+            memContents = getMemContents(path, memLocation, l)
             toPrint += Support.buffer("\(memLocation): \(memContents)", 23) + l.lineText + "\n"
             for i in 0..<l.tokens.count {
                 switch l.tokens[i].type {
@@ -311,7 +334,7 @@ extension Assembler {
                 case .ImmediateTuple: memLocation += 5
                 case .ImmediateInteger: memLocation += 1
                 case .Label:
-                    if l.chunks[i - 1] != ".start" {memLocation += 1}
+                    if l.chunks[i - 1] != ".start" && l.chunks[i - 1] != ".Start" {memLocation += 1}
                 case .Instruction: memLocation += 1
                 case .Register: memLocation += 1
                 default: memLocation += 0
